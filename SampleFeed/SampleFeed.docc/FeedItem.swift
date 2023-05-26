a19bf051177c741d9fbc696a31f510f684105a5a
Zara Davtian <zara.davtian@irlix.ru>
2023-05-26 17:43:31 +0400

    Add SampleFeed framework

---
+++ SampleFeed/SampleFeed.docc/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  
+//
+//  Created by Zara Davtian on 26.05.23.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+
+}
---
+++ SampleFeed/SampleFeed.h
@@ -0,0 +1,18 @@
+//
+//  SampleFeed.h
+//  SampleFeed
+//
+//  Created by Zara Davtian on 26.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFeed.
+FOUNDATION_EXPORT double SampleFeedVersionNumber;
+
+//! Project version string for SampleFeed.
+FOUNDATION_EXPORT const unsigned char SampleFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFeed/PublicHeader.h>
+
+
