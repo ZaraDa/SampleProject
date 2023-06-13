09cbf67f4a6f7b37741720a40ea516aa46c89578
Zara Davtian <zara.davtian@irlix.ru>
2023-06-13 14:32:11 +0400

    Add End to end tests target to separate them from unit tests

---
+++ SampleFeed/SampleFeedEndToEndTests/SampleFeedEndToEndTests.swift
@@ -0,0 +1,37 @@
+//
+//  SampleFeedEndToEndTests.swift
+//  SampleFeedEndToEndTests
+//
+//  Created by Zara Davtian on 13.06.23.
+//
+
+import XCTest
+import SampleFeed
+
+class SampleFeedEndToEndTests: XCTestCase {
+
+    /// matches fixed test account data
+    func test_endToEndTestServerGETFeedResult() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+
+        let exp = expectation(description: "wait for load completion")
+        var recievedResult: LoadFeedResult?
+        loader.load { result in
+            recievedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+
+        switch recievedResult {
+        case let .success(items)?:
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+        case let .failure(error)?:
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
+        }
+    }
+
+}
