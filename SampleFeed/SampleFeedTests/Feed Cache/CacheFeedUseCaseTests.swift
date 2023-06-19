9aa402618577bf49cffe142db4df96414ae47536
Zara Davtian <zara.davtian@irlix.ru>
2023-06-19 17:26:07 +0400

    Deletes cache on save command

--- SampleFeed/SampleFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ SampleFeed/SampleFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import SampleFeed
 
 
 class LocalFeedLoader {
@@ -15,10 +16,19 @@ class LocalFeedLoader {
         self.store = store
     }
 
+    func save(_ items: [FeedItem]) {
+
+        store.deleteCachedFeed()
+    }
+
 }
 
 class FeedStore {
     var deleteCachedFeedCount = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCount += 1
+    }
 }
 
 
@@ -30,6 +40,26 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deleteCachedFeedCount, 0)
+    }
+
+    func test_save_deletesCache() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem, uniqueItem]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCount, 1)
+
+    }
+
+    //MARK: - helpers
+
+    var uniqueItem: FeedItem {
+        FeedItem(id: UUID(),
+                 description: "any",
+                 location: "any",
+                 imageURL: anyURL)
     }
 
 }
