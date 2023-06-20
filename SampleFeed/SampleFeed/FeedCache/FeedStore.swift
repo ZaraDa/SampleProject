71f594ef157bf5b823d1411492c0cd49ca408489
Zara Davtian <zara.davtian@irlix.ru>
2023-06-20 14:22:09 +0400

    Move LocalFeedLoader and FeedStore to their own files in production modul

---
+++ SampleFeed/SampleFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  SampleFeed
+//
+//  Created by Zara Davtian on 20.06.23.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
---
+++ SampleFeed/SampleFeed/FeedCache/LocalFeedLoader.swift
@@ -0,0 +1,38 @@
+//
+//  LocalFeedLoader.swift
+//  SampleFeed
+//
+//  Created by Zara Davtian on 20.06.23.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private var currentDate: () -> Date
+
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items: items, completion: completion)
+            }
+        }
+    }
+
+    private func cache(items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        self.store.insert(items, timestamp: self.currentDate()) {[weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+
+}
