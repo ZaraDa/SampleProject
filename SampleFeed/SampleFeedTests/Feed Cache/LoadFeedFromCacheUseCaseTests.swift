0f5f84e879f335b4848b86e81c0c68ed308a66c7
Zara Davtian <zara.davtian@irlix.ru>
2023-06-21 14:10:35 +0400

    Extract FeedStoreSpy to separate file to prevent duplication

--- SampleFeed/SampleFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ SampleFeed/SampleFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -18,54 +18,6 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 
     //MARK: - helpers
 
-    class FeedStoreSpy: FeedStore {
-
-
-        enum RecievedMessage: Equatable {
-            case deleteCachedFeed
-            case insert([LocalFeedImage], Date)
-        }
-
-        var recievedMessages = [RecievedMessage]()
-
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            recievedMessages.append(.deleteCachedFeed)
-            deletionCompletions.append(completion)
-        }
-
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-
-        func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            recievedMessages.append(.insert(items, timestamp))
-            insertionCompletions.append(completion)
-        }
-
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-
-    }
-
-    var uniqueItem: FeedImage {
-        FeedImage(id: UUID(),
-                 description: "any",
-                 location: "any",
-                 url: anyURL)
-    }
-
     func makeSUT(currentDate: @escaping () -> Date = Date.init,
                  file: StaticString = #file,
                  line: UInt = #line) -> (LocalFeedLoader, FeedStoreSpy) {
