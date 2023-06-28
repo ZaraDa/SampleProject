ed89287cbe39361a7cd4d95ff1fbc7228a41e3e7
Zara Davtian <zara.davtian@irlix.ru>
2023-06-28 17:26:00 +0400

    Move CodableFeedStore to its own file

---
+++ SampleFeed/SampleFeed/FeedCache/CodableFeedStore.swift
@@ -0,0 +1,101 @@
+//
+//  CodableFeedStore.swift
+//  SampleFeed
+//
+//  Created by Zara Davtian on 28.06.23.
+//
+
+import Foundation
+
+
+public class CodableFeedStore: FeedStore {
+
+    private struct CodableFeedCache: Codable {
+       private  let images: [CodableFeedImage]
+       private  let timestamp: Date
+
+       init(images: [CodableFeedImage], timestamp: Date) {
+            self.images = images
+            self.timestamp = timestamp
+        }
+
+        func toFeedCache() -> FeedCache {
+            let localImages = images.map{ $0.toLocal() }
+            return FeedCache(images: localImages,
+                             timestamp: timestamp)
+        }
+    }
+
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+
+        func toLocal() -> LocalFeedImage {
+            LocalFeedImage(id: id,
+                           description: description,
+                           location: location,
+                           url: url)
+        }
+
+        static func toCodable(image: LocalFeedImage) -> CodableFeedImage {
+            CodableFeedImage(id: image.id,
+                             description: image.description,
+                             location: image.location,
+                             url: image.url)
+        }
+    }
+
+    public init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+
+    private let storeURL: URL
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        let fileManager = FileManager.default
+        guard fileManager.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+
+        do{
+            try fileManager.removeItem(at: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+
+
+    }
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(CodableFeedCache.self, from: data)
+            completion(.found(cache.toFeedCache()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    public func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        do {
+            let codableImages = items.map{ CodableFeedImage.toCodable(image: $0) }
+            let feedCache = CodableFeedCache(images: codableImages, timestamp: timestamp)
+
+            let encoder = JSONEncoder()
+            let encodedData = try! encoder.encode(feedCache)
+
+            try encodedData.write(to: storeURL)
+
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+}
