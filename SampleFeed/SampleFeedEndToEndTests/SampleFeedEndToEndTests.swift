7d330629cf8c70e947fe7d21772cae6f32243f60
Zara Davtian <zara.davtian@irlix.ru>
2023-07-04 14:50:46 +0400

    FeedLoader result changes

--- SampleFeed/SampleFeedEndToEndTests/SampleFeedEndToEndTests.swift
+++ SampleFeed/SampleFeedEndToEndTests/SampleFeedEndToEndTests.swift
@@ -25,14 +25,14 @@ class SampleFeedEndToEndTests: XCTestCase {
 
     //MARK: -- helpers
 
-    func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
+    func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "wait for load completion")
-        var recievedResult: LoadFeedResult?
+        var recievedResult: FeedLoader.Result?
         loader.load { result in
             recievedResult = result
             exp.fulfill()
