0c23caf532b425361d1b33032dbaa225e044a7a0
Zara Davtian <zara.davtian@irlix.ru>
2023-07-04 10:17:30 +0400

    Add separate target for cache integration tests to separate them from fast unit tests

---
+++ SampleFeed/SampleFeedCacheIntegrationTests/SampleFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  SampleFeedCacheIntegrationTests.swift
+//  SampleFeedCacheIntegrationTests
+//
+//  Created by Zara Davtian on 04.07.23.
+//
+
+import XCTest
+
+class SampleFeedCacheIntegrationTests: XCTestCase {
+
+    
+
+}
