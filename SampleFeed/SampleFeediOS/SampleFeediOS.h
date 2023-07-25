1d4ef7f77e5f0ddcb256d1f1bf3ce6467c8e487e
Zara Davtian <zara.davtian@irlix.ru>
2023-07-25 14:15:40 +0400

    Add SampleFeediOS framework (prod and test) targets for iOS platform-specific components

---
+++ SampleFeed/SampleFeediOS/SampleFeediOS.h
@@ -0,0 +1,18 @@
+//
+//  SampleFeediOS.h
+//  SampleFeediOS
+//
+//  Created by Zara Davtian on 25.07.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFeediOS.
+FOUNDATION_EXPORT double SampleFeediOSVersionNumber;
+
+//! Project version string for SampleFeediOS.
+FOUNDATION_EXPORT const unsigned char SampleFeediOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFeediOS/PublicHeader.h>
+
+
