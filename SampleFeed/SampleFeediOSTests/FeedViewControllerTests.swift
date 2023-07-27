c83b030762f76f4a22328c4955817c3f3147010d
Zara Davtian <zara.davtian@irlix.ru>
2023-07-27 19:28:24 +0400

    Load feed on view did load

---
+++ SampleFeed/SampleFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,54 @@
+//
+//  FeedViewControllerTests.swift
+//  SampleFeed
+//
+//  Created by Zara Davtian on 27.07.23.
+//
+
+import XCTest
+import UIKit
+
+final class FeedViewController: UIViewController {
+    var loader: FeedViewControllerTests.LoaderSpy?
+
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader?.load()
+    }
+}
+
+class FeedViewControllerTests: XCTestCase {
+
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        _  = FeedViewController(loader: loader)
+
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+
+        func load() {
+            loadCallCount += 1
+        }
+    }
+
+
+
+}
