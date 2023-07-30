0da7aeb2901d002be0547c1afffddca72ff2978b
Zara Davtian <zara.davtian@irlix.ru>
2023-07-30 15:27:51 +0400

    Render loaded feed on successful load completion

---
+++ SampleFeed/SampleFeediOS/FeedViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FeedViewController.swift
+//  SampleFeediOS
+//
+//  Created by Zara Davtian on 29.07.23.
+//
+
+import Foundation
+import UIKit
+import SampleFeed
+
+
+final public class FeedViewController: UITableViewController {
+    var loader: FeedLoader?
+    private var tableModel = [FeedImage]()
+
+    convenience public init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+            loader?.load {[weak self] result in
+                self?.tableModel = (try? result.get()) ?? []
+                self?.tableView.reloadData()
+                self?.refreshControl?.endRefreshing()
+            }
+        }
+
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
+    }
+
+    
+}
