2e5d04d2c1ce0af6d014eee1dcdbd00e7642bd12
Zara Davtian <zara.davtian@irlix.ru>
2023-08-01 13:29:03 +0400

    Extract UIImage test helper to a separate file

---
+++ SampleFeed/SampleFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,22 @@
+//
+//  TestHelpers.swift
+//  SampleFeediOSTests
+//
+//  Created by Zara Davtian on 01.08.23.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
